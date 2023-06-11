7b8c5a1118c69b641a620c07d3e0d78c83d10b6e
Daniel Magevski <daniel_magevski@hotmail.com>
2023-06-11 00:06:11 -0300

    Update About

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,16 @@
 // Insert custom javascript here.
+
+//img certifications 
+const content = document.querySelector('.content')
+
+const contentDivImg = document.querySelectorAll('p')
+
+const selectedFatherImgs = contentDivImg[1]
+
+selectedFatherImgs.classList.add('groupImg')
+
+//img languages and technologies
+
+const selectedFatherLanguages = contentDivImg[2]
+
+selectedFatherLanguages.classList.add('groupImg')
